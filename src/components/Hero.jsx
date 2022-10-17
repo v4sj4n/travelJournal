a4bba799f7344c13d6ad4cde5522e65ef9664d11
Vasjan Çupri <102856709+v4sj4n@users.noreply.github.com>
2022-10-17 19:43:59 +0200

    Changed margins and added hrs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,36 @@
 import locationIcon from "../assets/location-icon.png";
 
 function Hero(props) {
+  function Hrel(){
+    return(<hr className="hr--hero" />)
+  }
+  let isLastProp = false;
+  if (props.item.id != 2){
+    isLastProp = true
+  } 
   return (
-    <main>
-      <img
-        src={props.item.imageUrl}
-        alt={props.item.location}
-        className="hero--image"
-      />
-      <div className="hero">
-        <div className="hero--locationDiv">
-          <img src={locationIcon} alt="" />
-          <h3>{props.item.location.toUpperCase()}</h3>
-          <a href={props.item.googleMapsUrl}>View on Google Maps</a>
+    <div>
+      <main>
+        <img
+          src={props.item.imageUrl}
+          alt={props.item.location}
+          className="hero--image"
+        />
+        <div className="hero">
+          <div className="hero--locationDiv">
+            <img src={locationIcon} alt="" />
+            <h3>{props.item.location.toUpperCase()}</h3>
+            <a href={props.item.googleMapsUrl}>View on Google Maps</a>
+          </div>
+          <h1 className="hero--title">{props.item.title}</h1>
+          <h3 className="hero--dates">
+            {props.item.startDate} - {props.item.endDate}{" "}
+          </h3>
+          <p className="hero--paragraph">{props.item.description}</p>
         </div>
-        <h1 className="hero--title">{props.item.title}</h1>
-        <h3 className="hero--dates">
-          {props.item.startDate} - {props.item.endDate}{" "}
-        </h3>
-        <p className="hero--paragraph">{props.item.description}</p>
-      </div>
-    </main>
+      </main>
+     {isLastProp && <Hrel />} 
+    </div>
   );
 }
 
